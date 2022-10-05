e1acc934745996383f4f56929191cdacd42f514a
Your Name <you@example.com>
2022-10-05 13:52:35 +0530

    Added primeOrComposite.py and added my name to contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1840,4 +1840,9 @@ contributors = [
     fullname: "Sabin Thapa",
     username: "https://github.com/sabin-thapa",
   },
+  {
+    id:368,
+    fullname: "Shashwat Shivam",
+    username: "https://github.com/Shashwat49",
+  },
 ];
